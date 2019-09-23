eeecdd6cb719f92f4b39433e47bb5c90fbdc0bae
Kevin Urgilés <kevinurgiles1996@gmail.com>
2019-09-23 08:41:34 -0500

    Components, states & props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    loading: false,
+    users: []
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
-        <h1>Hello from React</h1>
+        <Navbar />
+        <div className='container'>
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
       </div>
     );
   }
